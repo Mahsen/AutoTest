0caad7eb4103a9693b2ef6ee10cfc1613c6527b8
Mohammad Lotfi <not.select@gmail.com>
2024-02-24 08:57:38 +0330

    Add config module

---
+++ Application/Module/Config.hpp
@@ -0,0 +1,17 @@
+#ifndef __CONFIG_HPP
+#define __CONFIG_HPP
+//----------------------------------------------------------
+#include "define.h"
+#include "media.hpp"
+//----------------------------------------------------------
+class CONFIG
+{
+	private:
+
+	public:
+
+		void Init(void);
+
+};
+extern CONFIG Config;
+#endif
