ffa4fdaf80c87a4d845ba53ce1c40608c299c958
Mahsen <not.select@gmail.com>
2024-05-27 14:47:09 +0330

    Edit task listen from port tcp socket

--- Application/Driver/Lan.hpp
+++ Application/Driver/Lan.hpp
@@ -10,34 +10,25 @@
 class LAN
 {
 	private:
-		// Define objects that are statically allocated for worker thread 1
-		osRtxThread_t thread_tcb;
-		// Reserve two areas for the stacks of worker thread 1
-		// uint64_t makes sure the memory alignment is 8
-		uint64_t thread_stk[4096];
-		// Define the attributes which are used for thread creation
-		// Optional const saves RAM memory and includes the values in periodic ROM tests 
-		const osThreadAttr_t attr = {
-			"LAN_Thread",
-			osThreadJoinable,
-			&thread_tcb,
-			sizeof(thread_tcb),
-			&thread_stk[0],
-			sizeof(thread_stk),
-			osPriorityAboveNormal,
-			0
-		};
-		// Define ID object for thread
-		osThreadId_t Thread;		
+	
 	public:
+		struct struct_Listen {
+			struct {
+				U8 Data[512];
+				U32 Length;
+			} Buffer;
+			bool (*CallBack)(S32 Socket, U8* Data, U32* Length);
+			S32 Socket;
+			U32 Port;
+		} Listen[LAN_SIZEOF_LISTEN];
+		
 		NET_ADDR _IP_Client;
-		U8 NumberOfListen;
+		U32 NumberOfListen;
 		void Init(void);
-		MEDIA* Listen(uint16_t Port);
+		bool AddListen(uint16_t Port, bool (*CallBack)(S32 Socket, U8* Data, U32* Length));
 		void GetLocal(U8 *IP, U8 *Mask, U8 *GateWay, U8 *DNS1, U8 *DNS2);
 		bool SetLocal(U8 *IP, U8 *Mask, U8 *GateWay, U8 *DNS1, U8 *DNS2);
-		NET_ADDR* GetClient(void);
-		void Task(void);
+		NET_ADDR* GetClient(void);		
 };
 extern LAN Lan;
 #endif
