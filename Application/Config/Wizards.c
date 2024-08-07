19f7583a32b6fdf4b2cfddba80c37276f090e347
Mahsen <not.select@gmail.com>
2024-08-07 16:42:58 +0330

    Update some defines

--- Application/Config/Wizards.c
+++ Application/Config/Wizards.c
@@ -9,7 +9,7 @@
     Site : https://www.mahsen.ir
     Tel : +989124662703
     Email : info@mahsen.ir
-    Last Update : 2024/8/4
+    Last Update : 2024/8/7
 */
 /************************************************** Warnings **********************************************************/
 /*
@@ -28,21 +28,99 @@
 // <h> Config Wizards
 // 	<h> Application
 //  <s> Version Hardware 
-#define APPLICATION_VERSION_HARDWARE            	                   "v7.20240804"
+#define APPLICATION_VERSION_HARDWARE            	                         "v7.20240807"
 //  <s> Version Software
-#define APPLICATION_VERSION_SOFTWARE            	                   "v1.20240804"
-// 		<h> MEDIA_OP_1
-//			<o> Media
-//			<2=> UART2
-#define APPLICATION_MEDIA_OP_1_MEDIA                                  2
+#define APPLICATION_VERSION_SOFTWARE            	                         "v1.20240807"
+// 		<h> Board_1
+// 			<h> Media
+// 				<h> OP
+//					<o> UART <1-12>
+#define APPLICATION_STAND_BOARD_1_MEDIA_OP_UART                            1
+// 				</h>
+// 				<h> RS485
+//					<o> UART <1-12>
+#define APPLICATION_STAND_BOARD_1_MEDIA_RS485_UART                         4
+// 				</h>
+// 			</h>
+// 		</h>
+// 		<h> Board_2
+// 			<h> Media
+// 				<h> OP
+//					<o> UART <1-12>
+#define APPLICATION_STAND_BOARD_2_MEDIA_OP_UART                            5
+// 				</h>
+// 				<h> RS485
+//					<o> UART <1-12>
+#define APPLICATION_STAND_BOARD_2_MEDIA_RS485_UART                         6
+// 				</h>
+// 			</h>
+// 		</h>
+// 		<h> Board_3
+// 			<h> Media
+// 				<h> OP
+//					<o> UART <1-12>
+#define APPLICATION_STAND_BOARD_3_MEDIA_OP_UART                            7
+// 				</h>
+// 				<h> RS485
+//					<o> UART <1-12>
+#define APPLICATION_STAND_BOARD_3_MEDIA_RS485_UART                         8
+// 				</h>
+// 			</h>
+// 		</h>
+// 		<h> Board_4
+// 			<h> Media
+// 				<h> OP
+//					<o> UART <1-12>
+#define APPLICATION_STAND_BOARD_4_MEDIA_OP_UART                            9
+// 				</h>
+// 				<h> RS485
+//					<o> UART <1-12>
+#define APPLICATION_STAND_BOARD_4_MEDIA_RS485_UART                         12
+// 				</h>
+// 			</h>
 // 		</h>
 // 	</h>
 // 	<h> Driver
 // 		<h> Uart
-// 			<h> UART2
+// 			<h> UART1
 //			 <o> Boadrate
 //				<9600=> 9600
-#define DIVER_UART_UART2_BOADRATE                                     9600
+#define DIVER_UART_UART1_BOADRATE                                     9600
+// 			</h>
+// 			<h> UART4
+//			 <o> Boadrate
+//				<9600=> 9600
+#define DIVER_UART_UART4_BOADRATE                                     9600
+// 			</h>
+// 			<h> UART5
+//			 <o> Boadrate
+//				<9600=> 9600
+#define DIVER_UART_UART5_BOADRATE                                     9600
+// 			</h>
+// 			<h> UART6
+//			 <o> Boadrate
+//				<9600=> 9600
+#define DIVER_UART_UART6_BOADRATE                                     9600
+// 			</h>
+// 			<h> UART7
+//			 <o> Boadrate
+//				<9600=> 9600
+#define DIVER_UART_UART7_BOADRATE                                     9600
+// 			</h>
+// 			<h> UART8
+//			 <o> Boadrate
+//				<9600=> 9600
+#define DIVER_UART_UART8_BOADRATE                                     9600
+// 			</h>
+// 			<h> UART9
+//			 <o> Boadrate
+//				<9600=> 9600
+#define DIVER_UART_UART9_BOADRATE                                     9600
+// 			</h>
+// 			<h> UART12
+//			 <o> Boadrate
+//				<9600=> 9600
+#define DIVER_UART_UART12_BOADRATE                                     9600
 // 			</h>
 // 		</h>
 // 	</h>
